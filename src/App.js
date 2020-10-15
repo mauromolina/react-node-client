4cb674553fb856f04ff6f31e3e9cebfebe46c809
Mauro Molina <molinamauro2@gmail.com>
2020-10-15 15:14:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/layout/Header';
+import Nav from './components/layout/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header/>
+    <div className="grid contenedor contenido-principal">
+      <Nav/>
+      <main class="caja-contenido col-9">
+        
+      </main>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
